c91894af506dc89ec9134231f9b587889894e85f
Fabian H. <hensslerfabian@gmail.com>
2026-02-03 15:37:39 +0100

    implement factory pattern in gilded rose project

--- GildedRose/GildedRose.java
+++ GildedRose/GildedRose.java
@@ -1,21 +1,12 @@
 package gildedrose;
 
 class GildedRose {
-    private static final String AGED_BRIE = "Aged Brie";
-    private static final String BACKSTAGE = "Backstage passes to a TAFKAL80ETC concert";
-    private static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
 
     public static Item[] items;
 
     public static void updateQuality() {
         for (Item item : items) {
-            ItemUpdater updater = switch (item.name) {
-                case AGED_BRIE -> new AgedBrieUpdater();
-                case BACKSTAGE -> new BackstagePassUpdater();
-                case SULFURAS -> new SulfurasUpdater();
-                default -> new NormalItemUpdater();
-            };
-            updater.update(item);
+            ItemUpdaterFactory.forItem(item).update(item);
         }
     }
 }
---
+++ GildedRose/ItemUpdaterFactory.java
@@ -0,0 +1,16 @@
+package gildedrose;
+
+public class ItemUpdaterFactory {
+    private static final String AGED_BRIE = "Aged Brie";
+    private static final String BACKSTAGE = "Backstage passes to a TAFKAL80ETC concert";
+    private static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
+
+    static ItemUpdater forItem(Item item) {
+        return switch (item.name) {
+            case AGED_BRIE -> new AgedBrieUpdater();
+            case BACKSTAGE -> new BackstagePassUpdater();
+            case SULFURAS -> new SulfurasUpdater();
+            default -> new NormalItemUpdater();
+        };
+    }
+}
